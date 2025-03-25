be1c5f66be6d0dd91e801574f982326714912441
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-03-25 22:21:29 +0600

    initial code - provided by madam

---
+++ AirlineReservationSystem/src/DisplayClass.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public interface DisplayClass {
+
+    void displayRegisteredUsersForAllFlight();
+
+    void displayRegisteredUsersForASpecificFlight(String flightNum);
+
+    void displayHeaderForUsers(Flight flight, List<Customer> c);
+
+    void displayFlightsRegisteredByOneUser(String userID);
+
+}
