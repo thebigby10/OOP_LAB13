23715dc770b2f690ca63c5ae5fe4fcf365d58740
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-03-25 23:26:10 +0600

    seperated customr validation logic to CustomerValidator class

---
+++ AirlineReservationSystem/src/CustomerValidator.java
@@ -0,0 +1,11 @@
+public class CustomerValidator {
+
+    public boolean isEmailUnique(String email, List<Customer> customerCollection) {
+        for (Customer customer : customerCollection) {
+            if (customer.getEmail().equals(email)) {
+                return false; // Email already exists
+            }
+        }
+        return true; // Email is unique
+    }
+}
