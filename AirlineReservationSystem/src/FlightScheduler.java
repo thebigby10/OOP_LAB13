196e272576bf77de0fedc87885d4ad2d243636a9
thebigby10 <md.a_rahmanasif_x86-64@protonmail.com>
2025-03-26 00:12:43 +0600

    seperated flightschedule logic to seperate class

---
+++ AirlineReservationSystem/src/FlightScheduler.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Random;
+
+public class FlightScheduler {
+
+    public LocalDateTime scheduleFlight(int daysFromNow) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, daysFromNow);
+        calendar.add(Calendar.HOUR, new Random().nextInt(24)); // Randomize the hour
+        calendar.set(Calendar.MINUTE, (calendar.get(Calendar.MINUTE) * 3) - new Random().nextInt(45));
+
+        return LocalDateTime.ofInstant(calendar.toInstant(), java.time.ZoneId.systemDefault());
+    }
+}
